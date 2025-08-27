4dd468ea76cf350bf1e3588e02a21dcf8957ee47
Kamwanga Raheem <kamwangaraheem2050@gmail.com>
2025-08-27 23:57:07 +0300

    create configuration file

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getFunctions } from "firebase/functions";
+
+const firebaseConfig = {
+  // Your Firebase config
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const functions = getFunctions(app);
